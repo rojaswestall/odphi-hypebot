956457ad3d740eace8cd40b1af7a4dea0a6fcdb6
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-12-14 13:27:04 -0600

    adding ids

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ var config = {};
 
 config.bots = [
   {
-    botID: 'xxxxxxxxxx',
-    groupID: 'xxxxxxxx',
+    botID: '7f24e4269e4079e071fb210f0d',
+    groupID: '36865020',
     groupLocalID: '1', // an identifier for this group used in this application only
-    botName: 'mybot',
-    groupName: 'the-group-name' // optional
+    botName: 'Tests'
+    //groupName: 'the-group-name' // optional
   }
 ];
 
