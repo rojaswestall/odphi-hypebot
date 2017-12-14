cf8eb97ef9aa8e11e466e7acda767443c249ecce
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-12-14 08:03:35 -0600

    fabian special and starting time

--- bot.js
+++ bot.js
@@ -3,24 +3,58 @@ var HTTPS = require('https');
 
 var botID = process.env.BOT_ID;
 
+
+function findWord(message, word) {
+  var wrdlen = word.length
+  var result = false;
+  var lowerMessage = message.toLowerCase();
+  var partial
+
+  for(i = 0; i < message.length; i++) {
+    if(i+wrdlen <= message.length) {
+      partial = lowerMessage.substr(i,i+wrdlen)
+      console.log(partial)
+      if (partial === word) {
+        result = true;
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+  return result;
+}
+
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^HYPE ME$/;
+      hypeMeRegex = /^HYPE ME$/;
 
-  console.log(request)
+  console.log(request.text)
+//   worker: node worker.js
+// clock:  node clock.js
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && hypeMeRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
-  } else {
+  } 
+  else if(request.text && findWord(request.text, "fabian")) {
+    this.res.writeHead(200);
+    postMessageFabian();
+    this.res.end();
+  } 
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-
+var postOptions = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
 
 var phrases = Array(
   "KAPPAS KAPPAS TILL WE DIE",
@@ -28,30 +62,24 @@ var phrases = Array(
   "Too Hype Too Hype!",
   "Too Proud Too Proud!",
   "Who you wit!?!",
-  "Where are the Capri-Suns?!")
+  "Where are the Capri-Suns?!",
+  "Who’s gonna represent till they die?",
+  "Too Kute Too Kute",
+  "Sigma what, Sigma who??",
+  "Alpha Beta Gamma Delta Epsilon Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omicron Pi Rho Sigma Tau Upsilon Phi Chi Psi Omega Delta Phi!!!!",
+  "Hype yourself.")
 
+// userIDs
 // When it hits 8:07 “It’s time to get Hype Hype Hype Hype Hype Hype Hype Hype!!!!”
 
-function postMessageFabian() {
 
-}
-
-function postMessageNeos() {
-  
-}
-
-function postMessage() {
-  var botResponse, options, body, botReq;
+function postMessageFabianHype() {
+  var botResponse, body, botReq;
 
   //botResponse = cool();
 
   botResponse = phrases[Math.floor(Math.random()*phrases.length)]
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
 
   body = {
     "bot_id" : botID,
@@ -60,7 +88,76 @@ function postMessage() {
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
-  botReq = HTTPS.request(options, function(res) {
+  botReq = HTTPS.request(postOptions, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+
+}
+
+function postMessageFabian() {
+  var botResponse, body, botReq;
+
+  //botResponse = cool();
+
+  botResponse = "fuck fabian"
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(postOptions, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+
+}
+
+function postMessageNeos() {
+
+}
+
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  //botResponse = cool();
+
+  botResponse = phrases[Math.floor(Math.random()*phrases.length)]
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(postOptions, function(res) {
       if(res.statusCode == 202) {
         //neat
       } else {
