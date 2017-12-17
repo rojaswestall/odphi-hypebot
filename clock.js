d6e9bb674543e3ef892aa64d91d67f06700846a5
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-12-17 12:51:45 -0600

    different clock

--- clock.js
+++ clock.js
@@ -1,7 +1,7 @@
 const Worker  = require('./worker');
 var CronJob = require('cron').CronJob;
 
-new CronJob({
+var am = new CronJob({
   cronTime: "10 20 15 * * *", //AM 8:07:01
   onTick: Bot.sendMessage("It’s time to get Hype Hype Hype Hype Hype Hype Hype Hype!!!!"),
   start: true,
@@ -9,9 +9,12 @@ new CronJob({
   runOnInit: true
 });
 
-new CronJob({
+var pm = new CronJob({
   cronTime: "1 7 20 * * *", //PM 8:07:01
   onTick: Bot.sendMessage("It’s time to get Hype Hype Hype Hype Hype Hype Hype Hype!!!!"),
   start: true,
   timeZone: "America/Chicago"
 });
+
+// am.start();
+// pm.start();
